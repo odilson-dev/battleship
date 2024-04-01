be9348282dcfe57b60ff7d89b3338c34eab6fefa
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-04-01 13:25:57 -0400

    Add Player and ComputerPlayer class

---
+++ src/player.js
@@ -0,0 +1,48 @@
+class Player {
+  constructor(name, gameboard) {
+    this.name = name;
+    this.gameboard = gameboard;
+  }
+
+  attackOpponent(x, y) {
+    this.gameboard.receiveAttack(x, y);
+  }
+}
+
+class ComputerPlayer extends Player {
+  constructor(gameboard) {
+    super("Computer", gameboard);
+    this.previousAttacks = [];
+  }
+
+  generateRandomAttack() {
+    const x = Math.floor(Math.random() * this.gameboard.size);
+    const y = Math.floor(Math.random() * this.gameboard.size);
+    return [x, y];
+  }
+
+  attackOpponent() {
+    let [x, y] = this.generateRandomAttack();
+    // This loop ensures that the computer doesn't attack the same coordinates twice. It checks if the generated random coordinates [x, y] already exist in the previousAttacks array. If they do, it generates new random coordinates until it finds ones that haven't been attacked before.
+    while (
+      this.previousAttacks.some(([prevX, prevY]) => prevX === x && prevY === y)
+    ) {
+      [x, y] = this.generateRandomAttack();
+    }
+    this.previousAttacks.push([x, y]);
+    this.gameboard.receiveAttack(x, y);
+  }
+}
+
+// // Example usage:
+// const playerBoard = new GameBoard(10);
+// const computerBoard = new GameBoard(10);
+
+// const player = new Player("Player 1", computerBoard);
+// const computer = new ComputerPlayer(playerBoard);
+
+// // Player takes turn
+// player.attackOpponent(2, 3);
+
+// // Computer takes turn
+// computer.attackOpponent();
