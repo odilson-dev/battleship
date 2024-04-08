3649787a30fd71b5c07f7b0f36464f6cf067678a
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-04-08 17:27:44 -0400

    Add a dialog box with instructions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import { Game } from "./game";
 import "./style.css";
 
+const dialogElem = document.getElementById("dialog");
+const showBtn = document.querySelector(".show");
+const closeBtn = document.querySelector(".close");
+
+showBtn.addEventListener("click", () => {});
+
+closeBtn.addEventListener("click", () => {
+  dialogElem.close();
+});
+
 const battleshipGame = new Game();
 
 battleshipGame.setUpBoards();
 
 const shipDirectionButton = document.getElementById("ship-direction");
-
+dialogElem.showModal();
 shipDirectionButton.addEventListener("click", () => {
   if (shipDirectionButton.textContent == "horizontal") {
     shipDirectionButton.textContent = "vertical";
