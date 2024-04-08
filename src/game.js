c22cfa5f347b66e5503a9e7ebf4c27ac824739d3
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-04-08 17:47:50 -0400

    Prevent events to occurs when there is no more ships to place

--- src/game.js
+++ src/game.js
@@ -39,40 +39,42 @@ export class Game {
   }
   addMouseOverEffectOn(cell) {
     cell.addEventListener("mouseover", () => {
-      const dataX = cell.getAttribute("data-x");
-      const dataY = cell.getAttribute("data-y");
-      const shipDetails = this.playerShipNamesAndLength[0];
-      console.log(shipDetails);
-      const shipA = new Ship(...shipDetails);
-      shipA.direction = this.shipDirectionButton.textContent;
-      if (this.playerBoard.canThisShipBePlacedHere(shipA, dataY, dataX)) {
-        let posX = parseInt(dataX);
-        let posY = parseInt(dataY);
-        for (let i = 0; i < shipA.length; i++) {
-          const ship = this.playerBoardDOM.querySelector(
-            `td[data-x="${posX}"][data-y="${posY}"]`
-          );
+      if (this.playerShipNamesAndLength.length > 0) {
+        const dataX = cell.getAttribute("data-x");
+        const dataY = cell.getAttribute("data-y");
+        const shipDetails = this.playerShipNamesAndLength[0];
+        console.log(shipDetails);
+        const shipA = new Ship(...shipDetails);
+        shipA.direction = this.shipDirectionButton.textContent;
+        if (this.playerBoard.canThisShipBePlacedHere(shipA, dataY, dataX)) {
+          let posX = parseInt(dataX);
+          let posY = parseInt(dataY);
+          for (let i = 0; i < shipA.length; i++) {
+            const ship = this.playerBoardDOM.querySelector(
+              `td[data-x="${posX}"][data-y="${posY}"]`
+            );
 
-          ship.classList.add("ship-position-allowed");
-          if (shipA.direction === "horizontal") {
-            posX++;
-          } else {
-            posY++;
+            ship.classList.add("ship-position-allowed");
+            if (shipA.direction === "horizontal") {
+              posX++;
+            } else {
+              posY++;
+            }
           }
-        }
-      } else {
-        let posX = parseInt(dataX);
-        let posY = parseInt(dataY);
-        for (let i = 0; i < shipA.length; i++) {
-          const ship = this.playerBoardDOM.querySelector(
-            `td[data-x="${posX}"][data-y="${posY}"]`
-          );
+        } else {
+          let posX = parseInt(dataX);
+          let posY = parseInt(dataY);
+          for (let i = 0; i < shipA.length; i++) {
+            const ship = this.playerBoardDOM.querySelector(
+              `td[data-x="${posX}"][data-y="${posY}"]`
+            );
 
-          ship.classList.add("ship-position-prohibited");
-          if (shipA.direction === "horizontal") {
-            posX++;
-          } else {
-            posY++;
+            ship.classList.add("ship-position-prohibited");
+            if (shipA.direction === "horizontal") {
+              posX++;
+            } else {
+              posY++;
+            }
           }
         }
       }
@@ -80,42 +82,44 @@ export class Game {
   }
   addMouseOutEffectOn(cell) {
     cell.addEventListener("mouseout", () => {
-      const dataX = cell.getAttribute("data-x");
-      const dataY = cell.getAttribute("data-y");
+      if (this.playerShipNamesAndLength.length > 0) {
+        const dataX = cell.getAttribute("data-x");
+        const dataY = cell.getAttribute("data-y");
 
-      const shipDetails = this.playerShipNamesAndLength[0];
-      console.log(shipDetails);
-      const shipA = new Ship(...shipDetails);
-      shipA.direction = this.shipDirectionButton.textContent;
+        const shipDetails = this.playerShipNamesAndLength[0];
+        console.log(shipDetails);
+        const shipA = new Ship(...shipDetails);
+        shipA.direction = this.shipDirectionButton.textContent;
 
-      if (this.playerBoard.canThisShipBePlacedHere(shipA, dataY, dataX)) {
-        let posX = parseInt(dataX);
-        let posY = parseInt(dataY);
-        for (let i = 0; i < shipA.length; i++) {
-          const ship = this.playerBoardDOM.querySelector(
-            `td[data-x="${posX}"][data-y="${posY}"]`
-          );
-          ship.classList.remove("ship-position-allowed");
-          if (shipA.direction === "horizontal") {
-            posX++;
-          } else {
-            posY++;
+        if (this.playerBoard.canThisShipBePlacedHere(shipA, dataY, dataX)) {
+          let posX = parseInt(dataX);
+          let posY = parseInt(dataY);
+          for (let i = 0; i < shipA.length; i++) {
+            const ship = this.playerBoardDOM.querySelector(
+              `td[data-x="${posX}"][data-y="${posY}"]`
+            );
+            ship.classList.remove("ship-position-allowed");
+            if (shipA.direction === "horizontal") {
+              posX++;
+            } else {
+              posY++;
+            }
           }
-        }
-      } else {
-        let posX = parseInt(dataX);
-        let posY = parseInt(dataY);
+        } else {
+          let posX = parseInt(dataX);
+          let posY = parseInt(dataY);
 
-        for (let i = 0; i < shipA.length; i++) {
-          const ship = this.playerBoardDOM.querySelector(
-            `td[data-x="${posX}"][data-y="${posY}"]`
-          );
+          for (let i = 0; i < shipA.length; i++) {
+            const ship = this.playerBoardDOM.querySelector(
+              `td[data-x="${posX}"][data-y="${posY}"]`
+            );
 
-          ship.classList.remove("ship-position-prohibited");
-          if (shipA.direction === "horizontal") {
-            posX++;
-          } else {
-            posY++;
+            ship.classList.remove("ship-position-prohibited");
+            if (shipA.direction === "horizontal") {
+              posX++;
+            } else {
+              posY++;
+            }
           }
         }
       }
@@ -124,46 +128,48 @@ export class Game {
 
   addClickEffectOn(cell) {
     cell.addEventListener("click", () => {
-      const dataX = cell.getAttribute("data-x");
-      const dataY = cell.getAttribute("data-y");
+      if (this.playerShipNamesAndLength.length > 0) {
+        const dataX = cell.getAttribute("data-x");
+        const dataY = cell.getAttribute("data-y");
 
-      // Check if the ship can be placed at the position dataX and dataY
-      if (
-        this.playerBoard.canThisShipBePlacedHere(
-          new Ship(
-            ...this.playerShipNamesAndLength[0],
-            this.shipDirectionButton.textContent
-          ),
-          dataY,
-          dataX
-        )
-      ) {
-        const shipDetails = this.playerShipNamesAndLength.shift();
-        console.log(shipDetails);
-        const shipA = new Ship(...shipDetails);
-        shipA.direction = this.shipDirectionButton.textContent;
+        // Check if the ship can be placed at the position dataX and dataY
+        if (
+          this.playerBoard.canThisShipBePlacedHere(
+            new Ship(
+              ...this.playerShipNamesAndLength[0],
+              this.shipDirectionButton.textContent
+            ),
+            dataY,
+            dataX
+          )
+        ) {
+          const shipDetails = this.playerShipNamesAndLength.shift();
+          console.log(shipDetails);
+          const shipA = new Ship(...shipDetails);
+          shipA.direction = this.shipDirectionButton.textContent;
 
-        if (this.playerBoard.canThisShipBePlacedHere(shipA, dataY, dataX)) {
-          this.playerBoard.placeShip(shipA, dataX, dataY);
+          if (this.playerBoard.canThisShipBePlacedHere(shipA, dataY, dataX)) {
+            this.playerBoard.placeShip(shipA, dataX, dataY);
 
-          let posX = parseInt(dataX);
-          let posY = parseInt(dataY);
-          for (let i = 0; i < shipA.length; i++) {
-            const ship = this.playerBoardDOM.querySelector(
-              `td[data-x="${posX}"][data-y="${posY}"]`
-            );
-            ship.classList.remove("ship-position-allowed");
+            let posX = parseInt(dataX);
+            let posY = parseInt(dataY);
+            for (let i = 0; i < shipA.length; i++) {
+              const ship = this.playerBoardDOM.querySelector(
+                `td[data-x="${posX}"][data-y="${posY}"]`
+              );
+              ship.classList.remove("ship-position-allowed");
 
-            ship.classList.add("ship");
-            if (shipA.direction === "horizontal") {
-              posX++;
-            } else {
-              posY++;
+              ship.classList.add("ship");
+              if (shipA.direction === "horizontal") {
+                posX++;
+              } else {
+                posY++;
+              }
             }
+            document.getElementById(
+              "ship-left-to-place"
+            ).textContent = `${this.playerShipNamesAndLength.length} ships left to place`;
           }
-          document.getElementById(
-            "ship-left-to-place"
-          ).textContent = `${this.playerShipNamesAndLength.length} ships left to place`;
         }
       }
     });
--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import { Game } from "./game";
 import "./style.css";
 
 const dialogElem = document.getElementById("dialog");
-const showBtn = document.querySelector(".show");
 const closeBtn = document.querySelector(".close");
-
-showBtn.addEventListener("click", () => {});
 
 closeBtn.addEventListener("click", () => {
   dialogElem.close();
